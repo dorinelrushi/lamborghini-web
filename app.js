35cddfe710b6ddee47437d0ec736f049392c3db0
dorinel.js <38764706+dorinelrushi@users.noreply.github.com>
2020-09-04 22:09:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+var demo = document.getElementById("demo");
+var yellow = document.getElementById("yellow");
+var orange = document.getElementById("orange");
+var red = document.getElementById("red");
+var purple = document.getElementById("purple");
+var blue = document.getElementById("blue");
+var green = document.getElementById("green");
+
+yellow.addEventListener("click",function(){
+    demo.src = "./photo/lm.png";
+})
+
+orange.addEventListener("click",function(){
+    demo.src = "./photo/orange.png";
+})
+
+red.addEventListener("click",function(){
+    demo.src = "./photo/red.png";
+})
+
+purple.addEventListener("click",function(){
+    demo.src = "./photo/purple.png";
+})
+
+blue.addEventListener("click",function(){
+    demo.src = "./photo/blue.png";
+})
+
+green.addEventListener("click",function(){
+    demo.src = "./photo/green.png";
+})
+
